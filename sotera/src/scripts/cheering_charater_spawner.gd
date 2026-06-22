1924f465f75042d60ffd156662ff071969be67df
Yoyo <yoav.cohen.99999@gmail.com>
2026-06-22 18:19:44 +0300

    add theater and a cheering character spawner

--- sotera/src/scripts/cheering_charater_spawner.gd
+++ sotera/src/scripts/cheering_charater_spawner.gd
@@ -17,12 +17,29 @@ func _ready() -> void:
 	max_characters = seats.size() + 5
 	await get_tree().physics_frame
 	for i in range(randi_range(min_starters_cheering, max_starters_cheering)):
+		print("i: " + str(i))
 		on_character_should_spawen()
 
 func on_character_should_spawen() -> void:
-	print(current_character_count)
 	if current_character_count >= max_characters:
 		return
+
+	var free_seat: Vector2 = find_free_seat()
+	if free_seat != Vector2.ZERO:
+		spawn_cheering_character(free_seat)
+	else:
+		print("all seats are taken")
+
+func spawn_cheering_character(spawn_position) -> void:
+	var cheering_charcater: Node2D = cheering_character_scene.instantiate()
+	cheering_charcater.global_position = spawn_position
+	cheering_charcater.cheering_character_spawner = self
+	cheering_charcater.seat = spawn_position
+	add_child(cheering_charcater)
+	current_character_count += 1
+	timer.wait_time = randi_range(min_wait_time, max_wait_time)
+
+func find_free_seat() -> Vector2:
 	var free_seats: Array[Vector2] = seats.keys()
 	var free_seat: Vector2
 	
@@ -30,22 +47,10 @@ func on_character_should_spawen() -> void:
 		free_seat = free_seats[randi_range(0, free_seats.size() - 1)]
 		if not seats[free_seat]:
 			seats[free_seat] = true
-			spawn_cheering_character(free_seat)
-			print("spawning caracter on seat")
-			return
+			return free_seat
 
-		free_seats.erase(free_seat)
-
-	spawn_cheering_character(Vector2(randi_range(0, 1800), randi_range(0, 600)))
-	free_seats.erase(free_seat)
-
-func spawn_cheering_character(spawn_position) -> void:
-	var cheering_charcater: Node2D = cheering_character_scene.instantiate()
-	cheering_charcater.global_position = spawn_position
-	cheering_charcater.cheering_character_spawner = self
-	add_child(cheering_charcater)
-	current_character_count += 1
-	timer.wait_time = randi_range(min_wait_time, max_wait_time)
+	return Vector2.ZERO
 
 func change_seat_state(seat: Vector2, is_block: bool):
-	if seat.
+	if seats.has(seat):
+		seats[seat] = is_block
--- sotera/src/scripts/theater.gd
+++ sotera/src/scripts/theater.gd
@@ -4,8 +4,9 @@ extends Node2D
 
 @export var space_between_seats_x: int
 @export var space_between_seats_y: int
-@export var texture_whidth: int = 50
-@export var texture_height: int = 50
+@export var seat_whidth: int = 50
+@export var seat_height: int = 50
+@export var theater_whidth = 1920
 
 #seat poseation and is seat taken
 var seats: Dictionary[Vector2, bool]
@@ -18,13 +19,14 @@ func _ready() -> void:
 	on_start()
 	cheering_charater_spawner.seats = seats
 	cheering_charater_spawner.max_characters = seats.size() + 5
+	print(seats.size())
 
 func on_start() -> void:
-	num_of_seats_per_row = int(1920 / (space_between_seats_x + texture_whidth)) + 2
-	var seat_poseation: Vector2 =  Vector2(0 - space_between_seats_x - texture_whidth, 100)
+	num_of_seats_per_row = int(theater_whidth / (space_between_seats_x + seat_whidth)) + 2
+	var seat_poseation: Vector2 =  Vector2(0 - space_between_seats_x - seat_whidth, 100)
 	for i in range(3):
 		for seat in range(num_of_seats_per_row):
 			seats[seat_poseation] = false
-			seat_poseation.x += texture_whidth + space_between_seats_x
-		seat_poseation.y += space_between_seats_y + texture_height
-		seat_poseation.x = 0 -space_between_seats_x - texture_whidth
+			seat_poseation.x += seat_whidth + space_between_seats_x
+		seat_poseation.y += space_between_seats_y + seat_height
+		seat_poseation.x = 0 -space_between_seats_x - seat_whidth
