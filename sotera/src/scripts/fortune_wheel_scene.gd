32bcd24cca9f77b4abd7988091e78d2b0b6737d2
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2026-06-26 16:35:12 +1200

    remove old stage exit and redo script, cause im silly

---
+++ sotera/src/scripts/fortune_wheel_scene.gd
@@ -0,0 +1,11 @@
+extends Node
+
+@export var curtains: CurtainSystem
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	curtains.open_full()
+	MusicPlayer.play_track(MusicPlayer.STAGE_MUSIC, -9.0)
+
+func _exit_tree() -> void:
+	MusicPlayer.stop_track(2.0)
