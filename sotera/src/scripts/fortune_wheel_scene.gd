2ed2b76db6f6fba9921f4eca22e97bdf33501c1e
DefinitelyMaybe <DefinitelyMaybe@users.noreply.github.com>
2026-06-26 18:06:09 +1200

    make a dialogue prefab. remove intro scene. Create easy function call.

--- sotera/src/scripts/fortune_wheel_scene.gd
+++ sotera/src/scripts/fortune_wheel_scene.gd
@@ -5,7 +5,8 @@ extends Node
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	curtains.open_full()
-	MusicPlayer.play_track(MusicPlayer.STAGE_MUSIC, -9.0)
+	MusicPlayer.play_track(MusicPlayer.STAGE_MUSIC)
+	$"DialogueText".start_next_dialog()
 
 func _exit_tree() -> void:
 	MusicPlayer.stop_track(2.0)
